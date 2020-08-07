0f1129efd9a293b21009578ee976a7b2e239a955
Justin Lohner <lohner4@gmail.com>
2020-08-07 10:00:32 -0600

    got ex2 file tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,18 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
-
+	def __init__(self, source, destination):
+		self.source = source
+		self.destination = destination
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+	mem = {}
+	route = []
+	for t in tickets:
+		mem[t.source] = t.destination
+	for i in range(length):
+		if len(route) <= 0:
+			route.append(mem["NONE"])
+		else:
+			route.append(mem[route[-1]])
 
-    return route
+	return route
