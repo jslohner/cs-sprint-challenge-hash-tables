2499033d62e8fd49b115bfc195ebd9b1e296f23b
Justin Lohner <lohner4@gmail.com>
2020-08-07 11:53:11 -0600

    got ex5 file tests passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,25 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return result
-
+	file_mem = {}
+	query_mem = {}
+	result = []
+	for f in files:
+		if (f.split('/')[-1]) not in file_mem:
+			file_mem[f.split('/')[-1]] = []
+		file_mem[f.split('/')[-1]].append(f)
+	for q in queries:
+		if q in file_mem:
+			result.extend(file_mem[q])
+	return result
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+	files = [
+		'/bin/foo',
+		'/bin/bar',
+		'/usr/bin/baz'
+	]
+	queries = [
+		"foo",
+		"qux",
+		"baz"
+	]
+	print(finder(files, queries))
