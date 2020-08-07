946d15c23dc1913b84ecd36d9a988202b7f17080
Justin Lohner <lohner4@gmail.com>
2020-08-07 11:30:37 -0600

    got ex3 file tests passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,28 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+	mem = {}
+	mem2 = {}
+	rtn = {}
+	for i in range(len(arrays)):
+		mem[i] = arrays[i]
+	for key in mem:
+		for num in mem[key]:
+			if num in mem2:
+				rtn[num] = 'intersects'
+				mem2[num] += 1
+			mem2[num] = 1
+			# if num not in mem2:
+			# 	mem2[num] = 0
+			# mem2[num] += 1
 
-    return result
+	return [k for k in rtn]
+	# return [k for k,v in mem2.items() if v > 1]
 
 
 if __name__ == "__main__":
-    arrays = []
+	arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+	arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+	arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+	arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+	print(intersection(arrays))
